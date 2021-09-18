9b06931072b90f79b86746740cf23daeb4368099
= <adithyanas1@gmail.com>
2021-09-18 16:55:55 +0530

    server

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const Logger = require("./logger");
-const fs = require("fs");
+const http = require("http");
 const path = require("path");
-const dataPath = "./data";
-const includes = fs.existsSync(dataPath);
-console.log(includes);
-const writeToFile = (data) => {
-	if (includes) {
-		fs.appendFile(
-			path.join(__dirname, "/data", "final_data.txt"),
-			JSON.stringify(data),
-			(err) => {
-				if (err) throw err;
-				console.log(" appended");
-			}
-		);
-	} else {
-		fs.mkdir(path.join(__dirname, "/data"), {}, (err) => {
-			if (err) throw err;
-			console.log("Folder Created");
-		});
-		fs.writeFile(
-			path.join(__dirname, "/data", "final_data.txt"),
-			JSON.stringify(data),
-			(err) => {
-				if (err) throw err;
-				console.log("Data added");
-			}
-		);
+const fs = require("fs");
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+	if (req.url === "/") {
+		res.end("<h1>Zoyo</h1>");
 	}
-};
-const logger = new Logger();
-logger.on("message", (data) => writeToFile(data));
-logger.log("Bro");
+});
+
+server.listen(PORT, () => {
+	console.log(`Server Running on port: ${PORT}`);
+});
--- logger.js
+++ logger.js
@@ -7,4 +7,37 @@ class Logger extends EventEmitter {
 		this.emit("message", { id: uuid.v4(), msg });
 	}
 }
-module.exports = Logger;
+const Logger = require("./logger");
+const fs = require("fs");
+const path = require("path");
+const dataPath = "./data";
+const includes = fs.existsSync(dataPath);
+console.log(includes);
+const writeToFile = (data) => {
+	if (includes) {
+		fs.appendFile(
+			path.join(__dirname, "/data", "final_data.txt"),
+			JSON.stringify(data),
+			(err) => {
+				if (err) throw err;
+				console.log(" appended");
+			}
+		);
+	} else {
+		fs.mkdir(path.join(__dirname, "/data"), {}, (err) => {
+			if (err) throw err;
+			console.log("Folder Created");
+		});
+		fs.writeFile(
+			path.join(__dirname, "/data", "final_data.txt"),
+			JSON.stringify(data),
+			(err) => {
+				if (err) throw err;
+				console.log("Data added");
+			}
+		);
+	}
+};
+const logger = new Logger();
+logger.on("message", (data) => writeToFile(data));
+logger.log("Bro");
